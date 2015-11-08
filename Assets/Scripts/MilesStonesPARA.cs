032ce91118146725f3b303f2e8a0f10f6e79ab85
max89max <maxime.larivierem@gmail.com>
2015-11-08 16:08:55 +0100

    Input des boutons du joueurs

---
+++ Assets/Scripts/MilesStonesPARA.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MilesStonesPARA : MonoBehaviour {
+
+	[SerializeField] private float timeMinToFinish;
+	[SerializeField] public GameObject Respawn;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
