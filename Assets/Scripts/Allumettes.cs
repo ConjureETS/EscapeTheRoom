c5a8b5d9fa5544869daed5daefcb89af02507aa9
max89max <maxime.larivierem@gmail.com>
2015-11-08 16:00:26 +0100

    Objets briquet et Allumette

---
+++ Assets/Scripts/Allumettes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Allumettes : MonoBehaviour {
+
+	public float m_Duree=500;
+	public bool activer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() 
+	{
+		if (activer)
+		{
+			m_Duree--;
+			print("SA brule, mais moins");
+		}
+	}
+}
