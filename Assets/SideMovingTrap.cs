c2362e404b92656e997dce04bee899c9f7949b7a
unknown <gallanker@gmail.com>
2015-11-08 12:11:26 -0500

    3rd scrub trap mostly done

--- Assets/SideMovingTrap.cs
+++ Assets/SideMovingTrap.cs
@@ -5,12 +5,13 @@ public class SideMovingTrap : MonoBehaviour {
 
     public float force;
     private Rigidbody rb;
-    bool addLeft = false;
-
+    //bool addLeft = false;
+    private Transform cyl;
     // Use this for initialization
     void Start () {
         rb = this.GetComponent<Rigidbody>();
-        rb.AddForce(100, 0, 0);
+        //rb.AddForce(100, 0, 0);
+        cyl = this.transform.parent;
     }
 	
 	// Update is called once per frame
@@ -21,14 +22,17 @@ public class SideMovingTrap : MonoBehaviour {
     void FixedUpdate()
     {
         Vector3 vel = rb.velocity;
-        print(vel.magnitude);
-        
-             
+        //print(vel.magnitude);
+        float toBeApplied = Mathf.Sin(Time.time) * force;
+        //print(toBeApplied);
+        rb.AddForce(toBeApplied, 0, 0);
 
     }
 
-    void pushTheShit()
-    {
-
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            print("playerDed");
+            Destroy(other.gameObject);
+        }
     }
 }
