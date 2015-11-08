a9f1b6ae10c56f83a1c8b91ffc905b2b5a7faf41
Patrice Vignola <vignola.patrice@gmail.com>
2015-11-08 07:41:29 -0500

    Add WIP for working on another PC

---
+++ Assets/Scriptss/Test/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CameraMovement : MonoBehaviour
+{
+    public float AngularSpeed = 10;
+    public float Speed = 100;
+
+    private Transform _transform;
+    private Rigidbody _rb;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        HandleMovementInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float yAxisValue = Input.GetAxis("Vertical");
+        
+        _rb.velocity = new Vector3(xAxisValue, _rb.velocity.y, yAxisValue) * Speed * Time.fixedDeltaTime;
+
+        _rb.angularVelocity = new Vector3(0f, Input.GetAxis("Mouse X"), 0f) * AngularSpeed;
+    }
+}
---
+++ Assets/Scriptss/Test/CameraRotation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float AngularSpeed = 10;
+    public float MaxYAngle = 290;
+    public float MinYAngle = 70;
+
+    private Transform _transform;
+
+    void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        HandleRotationInput();
+    }
+
+    private void HandleRotationInput()
+    {
+        Vector3 rotation = _transform.localEulerAngles + new Vector3(-Input.GetAxis("Mouse Y"), 0f, 0f) * AngularSpeed;
+
+        if (rotation.x < MaxYAngle && rotation.x > 180)
+	    {
+            rotation.x = MaxYAngle;
+	    }
+        else if (rotation.x > MinYAngle && rotation.x < 180)
+        {
+            rotation.x = MinYAngle;
+        }
+
+        _transform.localEulerAngles = rotation;
+    }
+}
