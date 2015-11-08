eeccb27de363069b65a6dd0490f41109bb05e1d3
Patrice Vignola <vignola.patrice@gmail.com>
2015-11-08 08:26:23 -0500

    Add the flickering light

--- Assets/Scripts/FlickeringLight.cs
+++ Assets/Scripts/FlickeringLight.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FlickeringLight : MonoBehaviour {
+public class FlickeringLight : MonoBehaviour
+{
+    public Light PointLight;
+    public float MinIntensity = 0.5f;
+    public float MaxIntensity = 0.7f;
+    public float FlickeringSpeed = 20f;
+	
+	void Update ()
+    {
+        float range = MaxIntensity - MinIntensity;
+        float AverageIntensity = MinIntensity + range / 2;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        Debug.Log(Mathf.Sin(Time.timeSinceLevelLoad));
+
+        PointLight.intensity = Random.Range(MinIntensity, MaxIntensity);
+    }
 }
